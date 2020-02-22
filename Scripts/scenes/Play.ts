c386570a7cfedcec06c4777853606c705b0a4779
Kissy1999 <35816345+Kissy1999@users.noreply.github.com>
2020-02-22 09:48:25 -0500

    Added private instance memebers

--- Scripts/scenes/Play.ts
+++ Scripts/scenes/Play.ts
@@ -1,8 +1,20 @@
+//Author: Kishore Jothinaryanan
+//COMP397_Midterm
+
+
 module scenes
 {
     export class Play extends objects.Scene
     {
         // PRIVATE INSTANCE MEMBERS
+        rollButton: objects.Button;
+        dice1:createjs.Bitmap;
+        dice2:createjs.Bitmap;
+        no1:objects.Label;
+        no2:objects.Label;
+        result1:number;
+        result2:number;
+    
  
 
         // PUBLIC PROPERTIES
